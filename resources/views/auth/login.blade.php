4d0446888bb2f88da86e94acd8a0539391f6857a
Nikko Adrian <nikkoadrian02@gmail.com>
2024-03-25 18:47:34 +0700

    edit welcome

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
 <div class="login-logo">
-<a href="#"><b>POS</b>Online</a>
+<a href="#"><b>{{ config('app.name'); }}</b></a>
 </div>
 <!-- /.login-logo -->
 <div class="card">

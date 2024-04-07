57247582a547220cbefd03f64ee8961fdf458bea
Nikko Adrian <nikkoadrian02@gmail.com>
2024-04-07 18:47:05 +0700

    cek

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -36,6 +36,7 @@ class NotaController extends Controller
     public function detail($id)
     {
         $nota = Nota::with('detailNota')->findOrFail($id);
+        dd($nota);
         return view('invoice', compact('nota'));
     }
 

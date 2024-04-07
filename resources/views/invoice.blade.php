801530631bb379d1478cb0779a915d40acaef5e8
Nikko Adrian <nikkoadrian02@gmail.com>
2024-04-07 18:54:20 +0700

    cek

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -3,22 +3,39 @@
 <head>
     <title>Invoice</title>
     <style>
+        @page {
+            size: 100mm 140mm;
+            margin: 5mm;
+        }
         body {
-            font-family: Courier New, monospace;
+            font-family: 'Arial', sans-serif;
+            margin: 2mm;
+            padding: 0;
+            color: #333;
+            font-size: 80%; /* Skala font ke 80% */
         }
         table {
             width: 100%;
             border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
+            margin-bottom: 10px;
         }
         th {
+            border-bottom: 1px solid black; /* Menetapkan garis bawah pada th */
+            padding: 8px;
+            text-align: left;
             background-color: #f2f2f2;
         }
+        td {
+            padding: 8px;
+            text-align: left;
+        }
         h1, h2 {
-            font-size: 20px;
+            font-size: 18px;
+            margin: 5px 0;
+            color: #333;
+        }
+        h2 {
+            margin-top: 15px;
         }
         .header {
             text-align: center;
@@ -26,9 +43,34 @@
         }
         .header h1 {
             margin: 0;
+            color: #555;
         }
         .header p {
             margin: 5px 0;
+            color: #777;
+        }
+        h5 {
+            font-size: 14px;
+            margin-top: 15px;
+            color: #777;
+        }
+        /* Style untuk label jumlah */
+        .amount-label {
+            font-weight: bold;
+            color: #333;
+            text-align: right;
+        }
+        /* Style untuk jumlah yang besar */
+        .amount {
+            font-weight: bold;
+            color: #333;
+            text-align: right;
+        }
+        /* Style untuk catatan */
+        .note {
+            font-size: 12px;
+            color: #777;
+            margin-top: 10px;
         }
     </style>
 </head>
@@ -36,39 +78,21 @@
     <div class="header">
         <h1>Invoice Toko Andreas</h1>
         <p>Alamat: Blok Karang Mulya RT 17 RW 05 Desa Jangga Kecamatan Losarang Kabupaten Indramayu 45253</p>
-        <p>Nomor HP: 08129002000</p>
     </div>
-
 
     <h2>Informasi Nota</h2>
     <table>
         <tr>
             <th>Id Invoice</th>
-            <td>{{ $nota->id }}</td>
-        </tr>
-        <tr>
             <th>Jenis Transaksi</th>
-            <td>{{ $nota->jenis_transaksi }}</td>
-        </tr>
-        <tr>
             <th>Kasir</th>
-            <td>{{ $nota->kasir }}</td>
-        </tr>
-        <tr>
             <th>Tanggal Transaksi</th>
-            <td>{{ \Carbon\Carbon::parse($nota->tanggal_transaksi)->locale('id_ID')->isoFormat('D MMMM YYYY') }}</td>
         </tr>
         <tr>
-            <th>Total Belanja</th>
-            <td>@rp($nota->total_belanja)</td>
-        </tr>
-        <tr>
-            <th>Bayar</th>
-            <td>@rp($nota->bayar)</td>
-        </tr>
-        <tr>
-            <th>Kembalian</th>
-            <td>@rp($nota->kembalian)</td>
+            <td>{{ $nota->id }}</td>
+            <td>{{ $nota->jenis_transaksi }}</td>
+            <td>{{ $nota->kasir }}</td>
+            <td>{{ \Carbon\Carbon::parse($nota->tanggal_transaksi)->locale('id_ID')->isoFormat('D MMMM YYYY, HH:mm') }}</td>
         </tr>
     </table>
 
@@ -93,7 +117,25 @@
             @endforeach
         </tbody>
     </table>
-    <h5>Catatan: Barang yang sudah dibeli tidak bisa dikembalikan</h5>
+
+    <table>
+        <tr>
+            <td class="amount-label">Total Belanja</td>
+            <td>:</td>
+            <td class="amount">@rp($nota->total_belanja)</td>
+        </tr>
+        <tr>
+            <td class="amount-label">Bayar</td>
+            <td>:</td>
+            <td class="amount">@rp($nota->bayar)</td>
+        </tr>
+        <tr>
+            <td class="amount-label">Kembalian</td>
+            <td>:</td>
+            <td class="amount">@rp($nota->kembalian)</td>
+        </tr>
+    </table>
+    <h5 class="note">Catatan: Barang yang sudah dibeli tidak bisa dikembalikan</h5>
     <script> window.print(); </script>
 </body>
 </html>

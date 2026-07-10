31bb2454fe69823abcb4d6890076471a32c0c932
Nikko Adrian <nikkoadrian02@gmail.com>
2026-07-10 19:39:36 +0700

    data barang

---
+++ database/migrations/2026_04_23_193301_create_pembelian_barang_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembelian_barang', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_supplier')
+                ->constrained('supplier')
+                ->onDelete('restrict');
+            $table->string('kode_pembelian')->unique();
+            $table->date('tanggal_pembelian');
+            $table->foreignId('id_barang')
+                ->constrained('data_barang')
+                ->onDelete('restrict');
+            $table->integer('qty');
+            $table->decimal('harga_modal', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
